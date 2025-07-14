3a97d19dfb3c7b8bb6cbfa68e5a9a9aad776b57f
Sandro Reis <consultorsandro@hotmail.com>
2025-07-14 09:42:02 -0300

    Class 349 Closures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::iter::Product;
+
 fn main() {
-    println!("Hello, world!");
+   let multiplier = 5;
+
+   let multiply_by = |value: i32| -> i32 {
+       return value * multiplier;
+   };
+
+    println!("{}", multiply_by(2));
+
+    let Product = | a: i32, b: i32 | -> i32 {
+        println!("Calculating product for you");
+        return a * b;
+    };
+    println!("{}", Product(3, 10));
+    println!("{}", Product(5, 8));
+
 }
