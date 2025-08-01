d5d7d792de95d0d3ec26381695bd2885ece8f6cd
Sandro Reis <consultorsandro@hotmail.com>
2025-08-01 08:57:45 -0300

    Class 352 Refatora a função main para remover código comentado e simplificar o uso de closures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+fn main() { // class 352
+   let multiplier = 5;
+
+   let multiply_by = |value| value * multiplier;
+   println!("{}", multiply_by(3 as u8));
+
+   let numbers = vec![1, 2, 3, 4, 5];
+   println!("{:?}", numbers);
+
+   let print_numbers = ||println!("{:?}", numbers);
+   print_numbers();
+   print_numbers();
+   println!("{:?}", numbers);
+}
+
+/*
 fn main() { // class 350
    let multiplier = 5;
 
@@ -7,3 +23,4 @@ fn main() { // class 350
    let mirror = |value| value;
    println!("{}", mirror("why so serious?")); // Closure with different types
 }
+*/
